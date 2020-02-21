564d90a241b520b80a40c89d67b2de2854c37d71
leeFernando <hi.fernando.lee@gmail.com>
2020-02-21 15:23:55 +0700

    Updates

---
+++ precache-manifest.a5c1ed6a44773a82fbaa36fe6dcfaf7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c686f90b639c94fbe27942f87aba57fb",
+    "url": "/guess-the-flag/index.html"
+  },
+  {
+    "revision": "bd788ad595067008e84a",
+    "url": "/guess-the-flag/static/css/main.c0fdeccf.chunk.css"
+  },
+  {
+    "revision": "ac16c044f10de96ab16d",
+    "url": "/guess-the-flag/static/js/2.e243c89c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/guess-the-flag/static/js/2.e243c89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd788ad595067008e84a",
+    "url": "/guess-the-flag/static/js/main.65fcea2b.chunk.js"
+  },
+  {
+    "revision": "85965a54d35d122b62d2",
+    "url": "/guess-the-flag/static/js/runtime-main.32553d83.js"
+  }
+]);
